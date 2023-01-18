0e2bf188ca9b631e231d2b997dd897111992b672
Eric <eric.tj1026@gmail.com>
2023-01-18 20:30:40 +0800

    add constructor

--- music.cpp
+++ music.cpp
@@ -2,10 +2,5 @@
 #include "song.hpp"
 
 int main() {
-
-  Song electric_relaxation;
-  electric_relaxation.add_artist("A Tribe Called Quest");
-  std::cout << electric_relaxation.get_artist();
-  
+  Song back_to_black("Back to Black","Amy Winehouse");
 }
-
--- song.cpp
+++ song.cpp
@@ -1,19 +1,12 @@
 #include "song.hpp"
 
-// add Song method definitions here:
-void Song::add_title(std::string new_title) {
+// add the Song constructor here:
 
-  title = new_title;
-
-}
-
+Song::Song(std::string new_title,std::string new_artist):title(new_title),artist(new_artist){}
 std::string Song::get_title() {
 
   return title;
 
-}
-void Song::add_artist(std::string new_artist){
-  artist = new_artist;
 }
 std::string Song::get_artist(){
   return artist;
