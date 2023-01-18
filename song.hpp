c4b8dca0aa05cf4d40247f294d3db6b3bffd66cc
Eric <eric.tj1026@gmail.com>
2023-01-18 20:34:09 +0800

    add destructor

--- song.hpp
+++ song.hpp
@@ -8,7 +8,7 @@ class Song {
 public:
   // Add a constructor here:
   Song(std::string new_title,std::string new_artist);
-  
+  ~Song();
   std::string get_title();
   
   std::string get_artist();
